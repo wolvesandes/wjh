2162715aa3a5e7d03f8f5370149327170ecf5d54
wolvesandes <1197591573@qq.com>
2023-07-02 07:36:33 +0800

    add test file

---
+++ 成绩转换.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int score;
+    std::cin >> score;
+
+    if (score >= 90) {
+        std::cout << "A" << std::endl;
+    } else if (score >= 80) {
+        std::cout << "B" << std::endl;
+    } else if (score >= 70) {
+        std::cout << "C" << std::endl;
+    } else if (score >= 60) {
+        std::cout << "D" << std::endl;
+    } else {
+        std::cout << "E" << std::endl;
+    }
+
+    return 0;
+}
